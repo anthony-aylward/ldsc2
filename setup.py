0fc25f48be48b0c6da2fe875c873786f693dc835
Anthony Aylward <anthonyaylward@Anthonys-MacBook-Air.local>
2019-08-01 11:18:03 -0700

    advance version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='ldsc2',
-    version='0.0.32',
+    version='0.0.33',
     author='Anthony Aylward',
     author_email='aaylward@eng.ucsd.edu',
     description='Simplify stratified LD score regression',
